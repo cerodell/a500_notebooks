20e681a4fcdb0efbf7efee7aed1f7f95e8eeee31
Chris Rodell <crodell@eoas.ubc.ca>
2019-11-28 19:56:38 -0800

    learning to apply conditions to var_ds

--- pro_cr/py/cadauw_xr.py
+++ pro_cr/py/cadauw_xr.py
@@ -22,9 +22,27 @@ var_ds = xr.open_zarr(filein)
 
 #var_ds.sel(time = slice('2001-01-02','2001-01-03'))
 
+tmep_10m = var_ds.TA[:,0].mean()
 
-#var_ds.where(var_ds. < 50, drop=True))
+
+hour = var_ds.groupby('time.hour').mean(dim='time')
+
+tmep_10m_hour = hour.TA[:,0]
 
 
+#inversion = hour.where(hour.TA[:,0] > hour.TA[:,6], drop=True)
+
+inversion = var_ds.where(var_ds.TA[:,0] > var_ds.TA[:,6], drop=True)
+
+#isel(time=0).plot(size=6)
+
+inversion.TA[:,6].plot(size=6, x='time')
+
+
+temp_2m = inversion.TA[:,6]
+
+
+
+#hour.TA.where(hour.TA[:,0] > hour.TA[:,6], drop=True).plot.scatter()
 
 #time = var_d
